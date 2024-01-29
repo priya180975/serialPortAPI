c3409b0d8213878fef4701fef5b0bc9b72f17cff
priya180975 <priyannayak100@gmail.com>
2024-01-29 21:32:16 +0530

    index file changed

--- api/index.py
+++ api/index.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
-import models #database structure
-from database import engine #db configuration
-from routers import users,login,weightdata
+# import models #database structure
+# from database import engine #db configuration
+# from routers import users,login,weightdata
 
 app=FastAPI()
 
-app.include_router(users.router)
-app.include_router(login.router)
-app.include_router(weightdata.router)
+# app.include_router(users.router)
+# app.include_router(login.router)
+# app.include_router(weightdata.router)
 
 @app.get('/')
 def main_page():
     return 'Hi'
 
 #convert models to db table
-models.Base.metadata.create_all(engine)
+# models.Base.metadata.create_all(engine)
